2c6583fbb89f1ea7773c9ed1c59a4cc6c88004c2
designtokenses <designtokens.es@gmail.com>
2024-11-23 20:27:50 +0100

    add config for .wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,19 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Mocha'
+
+config.font = wezterm.font ('JetBrainsMono Nerd Font Mono', { weight = 'Medium' })
+config.font_size = 18.0
+config.enable_tab_bar = false
+config.window_decorations = 'RESIZE'
+config.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
+
+-- and finally, return the configuration to wezterm
+return config
