5a9abe88cd327674d2f8ca03431caa96e9e4a079
designtokenses <designtokens.es@gmail.com>
2025-12-24 15:34:10 +0100

    add zsh to home-manager

--- nix-darwin/.config/nix-darwin/air/home/default.nix
+++ nix-darwin/.config/nix-darwin/air/home/default.nix
@@ -24,6 +24,40 @@
     enable = true;
   };
 
+  programs.zsh = {
+    enable = true;
+
+    shellAliases = {
+      ll = "ls -lah";
+      vim = "nvim";
+      v = "nvim";
+      tmux-sessionizer = "~/.local/bin/tmux-sessionizer";
+      lg = "lazygit";
+    };
+
+      initExtra = ''
+        # Aquí va lo que ANTES estaba en tu .zshrc
+        # (custom stuff que no tenga módulo HM)
+        source ~/.zsh_custom
+        source ~/.zsh_profile
+        source ~/.zsh_work
+      '';
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+  };
+
+  programs.carapace = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   home.file.".tmux.conf".source = ./config/tmux/.tmux.conf;
   xdg.configFile."nvim".source = ./config/nvim;
+  xdg.configFile."ghostty".source = ./config/ghostty;
 }
