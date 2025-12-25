58cb3ff7aaa91d663fb4281fb661c9f3ead33a84
designtokenses <designtokens.es@gmail.com>
2025-12-25 19:31:40 +0100

    update all config

--- nix-darwin/.config/nix-darwin/air/home/default.nix
+++ nix-darwin/.config/nix-darwin/air/home/default.nix
@@ -5,6 +5,12 @@
   home.stateVersion = "24.05";
   home.enableNixpkgsReleaseCheck = false;
 
+  home.sessionPath = [
+    "/etc/bin"
+    # "/etc/profiles/per-user/${config.home.username}/bin"
+    # "/run/current-system/sw/bin"
+  ];
+
   home.packages = with pkgs; [
     ripgrep
     zoxide
@@ -22,6 +28,12 @@
 
   programs.tmux = {
     enable = true;
+    mouse = true;
+
+    plugins = with pkgs.tmuxPlugins; [
+      tmux-sessionx
+      tmux-floax
+    ];
   };
 
   programs.zsh = {
@@ -35,12 +47,20 @@
       lg = "lazygit";
     };
 
-      initExtra = ''
-        # Aquí va lo que ANTES estaba en tu .zshrc
-        # (custom stuff que no tenga módulo HM)
-        source ~/.zsh_custom
+      initContent = ''
+        ## Esto lo tengo que ir migrando
         source ~/.zsh_profile
-        source ~/.zsh_work
+
+        # --- ssh-agent (start if needed) ---
+        if [[ -z "$SSH_AUTH_SOCK" ]]; then
+          # reuse a persistent agent socket
+          export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
+
+          if ! ssh-add -l >/dev/null 2>&1; then
+            rm -f "$SSH_AUTH_SOCK"
+            ssh-agent -a "$SSH_AUTH_SOCK" >/dev/null
+          fi
+        fi
       '';
   };
 
@@ -60,4 +80,9 @@
   home.file.".tmux.conf".source = ./config/tmux/.tmux.conf;
   xdg.configFile."nvim".source = ./config/nvim;
   xdg.configFile."ghostty".source = ./config/ghostty;
+  # home.file.".zprofile".text = ''
+  #   if [ -e /etc/static/zshrc ]; then
+  #     source /etc/static/zshrc
+  #   fi
+  # '';
 }
