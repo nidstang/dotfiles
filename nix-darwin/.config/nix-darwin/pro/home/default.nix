787cdce7f2952b812343150ba8aa28479a4fcae4
designtokenses <designtokens.es@gmail.com>
2026-03-18 13:54:49 +0100

    disable zsh from home manager

--- nix-darwin/.config/nix-darwin/pro/home/default.nix
+++ nix-darwin/.config/nix-darwin/pro/home/default.nix
@@ -13,6 +13,8 @@
     glow
   ];
 
+  programs.zsh.enable = false;
+
   programs.zsh.shellAliases = {
     rebuild-pro-system = "sudo darwin-rebuild switch --flake ~/dotfiles/nix-darwin/.config/nix-darwin/pro#pro";
     update-pro-system = "nix flake update && rebuild-pro-system";
